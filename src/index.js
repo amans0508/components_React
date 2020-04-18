381e5b4846db1a967169bf0c2708d50e4327abeb
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-19 01:50:30 +0530

    Adding Children props

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
+
 
 const App = () =>{
     return (
         <div className="ui container comments">
+            <ApprovalCard>
             <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentAdd="Hello" profileImage = {faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="Alex" timeAgo="Today at 5PM" commentAdd ="Hi!" profileImage = {faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="Jane" timeAge="Yesterday at 4PM" commentAdd = "Hey There!" profileImage = {faker.image.avatar()} />
+            </ApprovalCard>
         </div>
+
     );
 };
 
